2fd23ad6201061d4e1f5b96b77448714bb379869
Vinny-Oliver <135837794+Vinny-Oliver@users.noreply.github.com>
2025-02-02 20:49:40 -0300

    Incluindo explore nossos cursos e o entre em contato

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// script.js - Para interatividade, você pode adicionar alguma funcionalidade aqui, como um alerta quando o formulário for enviado
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+});
